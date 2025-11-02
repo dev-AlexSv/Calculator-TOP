d6b1b46830c595c679073b4a04c8c10b89df0e34
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-11-02 14:42:16 +0800

    Fix styles a bit and added Event Delegation
    by devAlex

--- code.js
+++ code.js
@@ -1,15 +1,73 @@
-function add() {
-
+function add(num1, num2) {
+    return num1 + num2;
 }
 
-function subract() {
-
+function subract(num1, num2) {
+    return num1 - num2;
 }
 
-function multiply() {
-
+function multiply(num1, num2) {
+    return num1 * num2;
 }
 
-function divide() {
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(num1, num2, operator, string) {
     
 }
+
+const digits = document.querySelector(".digits");
+const actions = document.querySelector(".actions");
+const displayInput = document.querySelector(".display-input")
+
+digits.addEventListener('click', (event) => {
+  // Check if the clicked element is a direct child of the parent
+  if (event.target.parentNode === digits) {
+    console.log(`Clicked on a direct child: ${event.target.textContent}`);
+    // Perform actions based on the specific child (e.g., using event.target.id or classList)
+    switch (event.target.textContent) {
+        case '0':
+            
+            break;
+        case '1':
+            alert("You entered one.");
+            break;
+        case '2':
+            alert("You entered two.");
+            break;
+        case '3':
+            alert("You entered three.");
+            break;
+        case '4':
+            alert("You entered four.");
+            break;
+        case '5':
+            alert("You entered five.");
+            break;
+        case '6':
+            alert("You entered six.");
+            break;
+        case '7':
+            alert("You entered seven.");
+            break;
+        case '8':
+            alert("You entered eight.");
+            break;
+        case '9':
+            alert("You entered nine.");
+            break;
+        default:
+            alert("Not a valid digit character.");         
+    }
+  }
+});
+
+actions.addEventListener('click', (event) => {
+  // Check if the clicked element is a direct child of the parent
+  if (event.target.parentNode === actions) {
+    console.log(`Clicked on a direct child: ${event.target.textContent}`);
+    // Perform actions based on the specific child (e.g., using event.target.id or classList)
+  }
+});
